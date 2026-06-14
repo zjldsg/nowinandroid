254ceae0a82a686176461cefa0cb01968096eaff
Li mei lin <1290999646@qq.com>
2026-06-14 23:03:22 +0800

    更新nowinandroid最终版：同步完整项目源码（排除build产物）

---
+++ nowinandroid最终版/build-logic/convention/src/main/kotlin/RootPlugin.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.google.samples.apps.nowinandroid.configureGraphTasks
+import com.google.samples.apps.nowinandroid.configureSpotlessForRootProject
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.configuration.BuildFeatures
+import javax.inject.Inject
+
+abstract class RootPlugin : Plugin<Project> {
+    @get:Inject abstract val buildFeatures: BuildFeatures
+
+    override fun apply(target: Project) {
+        require(target.path == ":")
+        if (!buildFeatures.isIsolatedProjectsEnabled()) {
+            target.subprojects { configureGraphTasks() }
+        }
+        target.configureSpotlessForRootProject()
+    }
+}
+
+private fun BuildFeatures.isIsolatedProjectsEnabled(): Boolean {
+    return isolatedProjects.active.getOrElse(false)
+}
---
+++ nowinandroid最终版/build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/NiaFlavor.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.ApplicationProductFlavor
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.ProductFlavor
+import org.gradle.kotlin.dsl.invoke
+
+@Suppress("EnumEntryName")
+enum class FlavorDimension {
+    contentType,
+}
+
+// The content for the app can either come from local static data which is useful for demo
+// purposes, or from a production backend server which supplies up-to-date, real content.
+// These two product flavors reflect this behaviour.
+@Suppress("EnumEntryName")
+enum class NiaFlavor(val dimension: FlavorDimension, val applicationIdSuffix: String? = null) {
+    demo(FlavorDimension.contentType, applicationIdSuffix = ".demo"),
+    prod(FlavorDimension.contentType),
+}
+
+fun configureFlavors(
+    commonExtension: CommonExtension,
+    flavorConfigurationBlock: ProductFlavor.(flavor: NiaFlavor) -> Unit = {},
+) {
+    commonExtension.apply {
+        FlavorDimension.entries.forEach { flavorDimension ->
+            flavorDimensions += flavorDimension.name
+        }
+
+        productFlavors {
+            NiaFlavor.entries.forEach { niaFlavor ->
+                register(niaFlavor.name) {
+                    dimension = niaFlavor.dimension.name
+                    flavorConfigurationBlock(this, niaFlavor)
+                    if (commonExtension is ApplicationExtension && this is ApplicationProductFlavor) {
+                        if (niaFlavor.applicationIdSuffix != null) {
+                            applicationIdSuffix = niaFlavor.applicationIdSuffix
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
